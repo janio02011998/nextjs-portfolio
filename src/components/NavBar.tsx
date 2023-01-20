1b74d0c94c6fcdaa6e9514e3957e1b87f312aad9
JANIO <janiocarvalhojr@gmail.com>
2023-01-20 11:46:57 -0300

    add tailwind and setups initials

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NavBar: React.FC = () => {
+  return (
+    <div className="fixed w-full h-20 shadow-xl z-[100]">
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default NavBar;

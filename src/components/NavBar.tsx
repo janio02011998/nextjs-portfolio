3ce10c26d01110a8aae6110341ddef6942016026
JANIO <janiocarvalhojr@gmail.com>
2023-01-20 12:45:20 -0300

    fix absolute path and add styles

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,14 @@
+import Image from 'next/image';
 import React from 'react';
 
 const NavBar: React.FC = () => {
   return (
     <div className="fixed w-full h-20 shadow-xl z-[100]">
-      <div>
-
+      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+        <Image src="/assets/navLogo.png" alt="/" width="125" height="50" />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default NavBar;
